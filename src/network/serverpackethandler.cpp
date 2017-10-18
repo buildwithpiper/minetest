513069264799cbdb3220d524066dad6d6b6facaf
Greg Guterman <greg@withpiper.com>
2017-10-18 13:45:05 -0700

    add on_interactnodes lua callback

--- src/network/serverpackethandler.cpp
+++ src/network/serverpackethandler.cpp
@@ -1102,6 +1102,7 @@ void Server::handleCommand_Interact(NetworkPacket* pkt)
 	RollbackScopeActor rollback_scope(m_rollback,
 			std::string("player:")+player->getName());
 
+
 	/*
 		0: start digging or punch object
 	*/
@@ -1323,6 +1324,8 @@ void Server::handleCommand_Interact(NetworkPacket* pkt)
 				client->ResendBlockIfOnWire(blockpos2);
 			}
 		}
+
+		m_script->on_interactnodes(p_under, playersao);
 	} // action == 3
 
 	/*
