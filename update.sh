2933c02f688a5f717853823377eb7fbd25d04fe0
hashcacher <gregoryguterman@gmail.com>
2017-12-01 12:40:25 -0800

    change pipercraft to piperonline change our tab to piper online add dev mode flag to main menu change menu tab name

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo Updating PiperCraft
-rsync -vcaiz --no-o --no-g --delay-updates --exclude .git rsync://updater.playpiper.com/pipercraft-master/* ~/pipercraft
+echo Updating PiperOnline
+rsync -vcaiz --no-o --no-g --delay-updates --exclude .git rsync://updater.playpiper.com/piperonline-master/* ~/piperonline
