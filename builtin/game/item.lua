a7c402c0a706cbc4b5a3ba002055d6e61419a211
Matt Quinn <mquinn1317@berkeley.edu>
2017-12-20 16:46:36 -0800

    Parcel protection

--- builtin/game/item.lua
+++ builtin/game/item.lua
@@ -275,7 +275,7 @@ function core.item_place_node(itemstack, placer, pointed_thing, param2,
 	local playername = user_name(placer)
 	local log = make_log(playername)
 
-	if minetest.is_protected(above, "place", {name=itemstack:get_name()}) then
+	if minetest.is_protected(above, placer:get_player_name(), {name=itemstack:get_name()}) then
 		return itemstack, false
 	end
 
@@ -551,7 +551,7 @@ function core.node_dig(pos, node, digger)
 		return
 	end
 
-	if is_protected(pos, diggername) then
+	if minetest.is_protected(pos, diggername) then
 		log("action", diggername
 				.. " tried to dig " .. node.name
 				.. " at protected position "
