d0262109a8c2aa37447885206de5dc953720f952
hashcacher <gregoryguterman@gmail.com>
2017-12-21 14:11:14 -0800

    Revert "make leave messages use nickname"
    This reverts commit dfc82811eec164b3744769d49a6d912d141aa8e5.

--- builtin/game/misc.lua
+++ builtin/game/misc.lua
@@ -49,13 +49,11 @@ function core.send_join_message(player_name)
 end
 
 function core.send_leave_message(player_name, timed_out)
-	if nicknames[player_name] then
-		local announcement = "*** " ..  nicknames[player_name] .. " left the game."
-		if timed_out then
-			announcement = announcement .. " (timed out)"
-		end
-		core.chat_send_all(announcement)
+	local announcement = "*** " ..  player_name .. " left the game."
+	if timed_out then
+		announcement = announcement .. " (timed out)"
 	end
+	core.chat_send_all(announcement)
 end
 
 core.register_on_joinplayer(function(player)
