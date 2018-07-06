a977fd190cf96cfcddd00a3aa1ae93139f671439
Dave Milici <milici.dave@gmail.com>
2018-07-06 12:28:31 -0700

    Added cmake toolchain file for ARM cross-compiler.

---
+++ toolchain.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(project /home/davemilici/Developer/Piper/workspace/internal)
+set(CMAKE_SYSROOT ${project}/erootfs)
+set(CMAKE_STAGING_PREFIX ${project}/erootfs)
+
+set(tools /usr/bin)
+set(CMAKE_C_COMPILER ${tools}/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/arm-linux-gnueabihf-g++)
+set(CMAKE_CXX_FLAGS "-march=armv8-a+crc -mtune=cortex-a53 -mfpu=neon-vfpv4")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
