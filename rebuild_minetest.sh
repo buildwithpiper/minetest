8ab86f67f7a119bb909919fd8279028807f314e9
Dave Milici <milici.dave@gmail.com>
2018-07-19 16:16:15 -0700

    build script modified for static irrlicht lib without EGL/GLES2

--- rebuild_minetest.sh
+++ rebuild_minetest.sh
@@ -9,13 +9,11 @@ cmake . -DCMAKE_BUILD_TYPE=Release \
 	-DLUA_INCLUDE_DIR=${EROOTFS}/usr/include/luajit-2.0 \
         -DENABLE_LUAJIT=YES \
 	-DRUN_IN_PLACE=TRUE \
-	-DENABLE_GLES=1 \
-	-DEGL_INCLUDE_DIR=${EROOTFS}/opt/vc/include \
-	-DOPENGLES2_INCLUDE_DIR=$EROOTFS/opt/vc/include \
+	-DENABLE_GLES=0 \
 	-DBUILD_SERVER=NO \
 	-DIRRLICHT_SOURCE_DIR=irrlicht/source/Irrlicht/ \
 	-DIRRLICHT_LIBRARY=irrlicht/lib/Linux/libIrrlicht.a \
 	-DIRRLICHT_INCLUDE_DIR=irrlicht/include/ \
-	-DCMAKE_EXE_LINKER_FLAGS='-L${EROOTFS}/opt/vc/lib -lbrcmEGL -lbrcmGLESv2 -lbcm_host -lvcos -lvchiq_arm'
+	-DCMAKE_EXE_LINKER_FLAGS=''
 
 make -j $(nproc || sysctl -n hw.ncpu || echo 2)
