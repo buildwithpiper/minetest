258f3efb2aa6d42f84e1dc70f3314707af9b4e23
Dave Milici <milici.dave@gmail.com>
2018-08-09 22:15:56 -0700

    Modify build for Irrlicht 1.9 GLES2 branch linking Broadcom EGL/GLES2 libs.

--- rebuild_minetest.sh
+++ rebuild_minetest.sh
@@ -10,10 +10,12 @@ cmake . -DCMAKE_BUILD_TYPE=Release \
         -DENABLE_LUAJIT=YES \
 	-DRUN_IN_PLACE=TRUE \
 	-DENABLE_GLES=1 \
+	-DEGL_INCLUDE_DIR=${EROOTFS}/opt/vc/include \
+	-DOPENGLES2_INCLUDE_DIR=${EROOTFS}/opt/vc/include \
 	-DBUILD_SERVER=NO \
 	-DIRRLICHT_SOURCE_DIR=irrlicht/source/Irrlicht/ \
 	-DIRRLICHT_LIBRARY=irrlicht/lib/Linux/libIrrlicht.so.1.9.0 \
 	-DIRRLICHT_INCLUDE_DIR=irrlicht/include/ \
-	-DCMAKE_EXE_LINKER_FLAGS='-L${EROOTFS}/usr/lib -lEGL -lGLESv2'
+	-DCMAKE_EXE_LINKER_FLAGS="-L${EROOTFS}/opt/vc/lib -lbrcmEGL -lbrcmGLESv2 -lbcm_host -lvcos -lvchiq_arm"
 
 make -j $(nproc || sysctl -n hw.ncpu || echo 2)
