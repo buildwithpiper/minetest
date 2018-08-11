476a49747869bddd46e79f89355a8fae0b5785f9
Dave Milici <dave@joinloop.com>
2018-08-11 12:48:40 -0700

    Selectable options for Mesa vs Broadcom EGL/GLES libs.

--- rebuild_minetest.sh
+++ rebuild_minetest.sh
@@ -4,18 +4,35 @@ set -o errtrace
 trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 set -o pipefail
 
+EGL_MESA=1
+EGL_BROADCOM=0
+
+if [ $EGL_BROADCOM -eq 1 ]; then
+	EGL_INC=${EROOTFS}/opt/vc/include
+	EGL_LIB=${EROOTFS}/opt/vc/lib
+	EGL_LINK="-L${EGL_LIB} -lbrcmEGL -lbrcmGLESv2 -lbcm_host -lvcos -lvchiq_arm"
+elif [ $EGL_MESA -eq 1 ]; then
+	EGL_INC=${EROOTFS}/usr/include
+	EGL_LIB=${EROOTFS}/usr/lib
+	EGL_LINK="-L${EGL_LIB} -lEGL -lGLESv2"
+else
+	EGL_INC=${EROOTFS}/usr/include
+	EGL_LIB=${EROOTFS}/usr/lib
+	EGL_LINK=""
+fi
+
 cmake . -DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
 	-DLUA_INCLUDE_DIR=${EROOTFS}/usr/include/luajit-2.0 \
         -DENABLE_LUAJIT=YES \
 	-DRUN_IN_PLACE=TRUE \
-	-DENABLE_GLES=1 \
-	-DEGL_INCLUDE_DIR=${EROOTFS}/opt/vc/include \
-	-DOPENGLES2_INCLUDE_DIR=${EROOTFS}/opt/vc/include \
+	-DENABLE_GLES=$EGL_MESA||$EGL_BROADCOM \
+	-DEGL_INCLUDE_DIR=$EGL_INC \
+	-DOPENGLES2_INCLUDE_DIR=$EGL_INC \
 	-DBUILD_SERVER=NO \
 	-DIRRLICHT_SOURCE_DIR=irrlicht/source/Irrlicht/ \
-	-DIRRLICHT_LIBRARY=irrlicht/lib/Linux/libIrrlicht.so.1.9.0 \
+	-DIRRLICHT_LIBRARY=irrlicht/lib/Linux/libIrrlicht.so \
 	-DIRRLICHT_INCLUDE_DIR=irrlicht/include/ \
-	-DCMAKE_EXE_LINKER_FLAGS="-L${EROOTFS}/opt/vc/lib -lbrcmEGL -lbrcmGLESv2 -lbcm_host -lvcos -lvchiq_arm"
+	-DCMAKE_EXE_LINKER_FLAGS="${EGL_LINK}"
 
 make -j $(nproc || sysctl -n hw.ncpu || echo 2)
